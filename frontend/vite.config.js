1d6920db91ed6e7ec9222dd8d5ee11639818b700
Jane1217 <yanxinch@sas.upenn.edu>
2025-07-16 22:37:14 +0800

    修改Vite本地配置，以避免跟vercel配置冲突

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs';
 import path from 'path';
 
+const isLocal = !process.env.VERCEL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,10 +12,12 @@ export default defineConfig({
     port: 3000,
     open: true,
     host: '0.0.0.0', // 允许局域网访问
-    https: {
-      key: fs.readFileSync(path.resolve(process.env.HOME, 'certs/vite.key')),
-      cert: fs.readFileSync(path.resolve(process.env.HOME, 'certs/vite.crt')),
-    },
+    ...(isLocal && {
+      https: {
+        key: fs.readFileSync(path.resolve(process.env.HOME, 'certs/vite.key')),
+        cert: fs.readFileSync(path.resolve(process.env.HOME, 'certs/vite.crt')),
+      }
+    }),
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
