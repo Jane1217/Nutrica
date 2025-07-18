d412b8a1ab0ab70c86f93d9289fd618dea11c121
Z091388 <1841231109@qq.com>
2025-07-19 02:59:27 +0800

    safari图标更新

--- frontend/src/pages/auth/pages/Account settings.jsx
+++ frontend/src/pages/auth/pages/Account settings.jsx
@@ -239,10 +239,39 @@ export default function AccountSettings({ userEmail }) {
         <div className={styles['account-info-box']}>
             <div className={styles.accountInfoBoxHeader}>
               <span className={`${styles.accountInfoBoxTitle} body1`}>
-                <img src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f30d.svg" alt="safari" className={styles.accountInfoBoxIcon} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className={styles.accountInfoBoxIcon}>
+                  <rect width="24" height="24" rx="8" fill="white"/>
+                  <path d="M21.7636 12.078C21.7636 13.364 21.5117 14.6374 21.0222 15.8255C20.5328 17.0136 19.8154 18.0932 18.911 19.0025C18.0067 19.9119 16.933 20.6332 15.7514 21.1253C14.5698 21.6175 13.3033 21.8708 12.0244 21.8708C9.44137 21.8708 6.96416 20.839 5.13771 19.0025C3.31125 17.166 2.28516 14.6752 2.28516 12.078C2.28516 9.48075 3.31125 6.98991 5.13771 5.15341C6.96416 3.3169 9.44137 2.28516 12.0244 2.28516C13.3033 2.28516 14.5698 2.53846 15.7514 3.03059C16.933 3.52272 18.0067 4.24406 18.911 5.15341C19.8154 6.06275 20.5328 7.1423 21.0222 8.33042C21.5117 9.51854 21.7636 10.792 21.7636 12.078Z" fill="url(#paint0_linear_660_3534)" stroke="#CDCDCD" strokeWidth="0.351543" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M20.999 12.0782C20.999 14.4719 20.0533 16.7675 18.37 18.4601C16.6866 20.1527 14.4035 21.1036 12.0229 21.1036C9.64234 21.1036 7.35924 20.1527 5.6759 18.4601C3.99256 16.7675 3.04688 14.4719 3.04688 12.0782C3.04688 9.68449 3.99256 7.38883 5.6759 5.69623C7.35924 4.00363 9.64234 3.05273 12.0229 3.05273C14.4035 3.05273 16.6866 4.00363 18.37 5.69623C20.0533 7.38883 20.999 9.68449 20.999 12.0782Z" fill="url(#paint1_radial_660_3534)"/>
+                  <path d="M13.0503 13.1586L10.998 10.9976L18.2783 6.07324L13.0503 13.1586Z" fill="#FF5150"/>
+                  <path d="M13.0498 13.1581L10.9975 10.9971L5.76953 18.0825L13.0498 13.1581Z" fill="#F1F1F1"/>
+                  <path opacity="0.243" d="M5.76953 18.083L13.0498 13.1586L18.2777 6.07324L5.76953 18.083Z" fill="#22221B"/>
+                  <defs>
+                    <filter id="filter0_f_660_3534" x="3.92587" y="4.26962" width="16.7762" height="16.4385" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                      <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                      <feGaussianBlur stdDeviation="1.27242" result="effect1_foregroundBlur_660_3534"/>
+                    </filter>
+                    <linearGradient id="paint0_linear_660_3534" x1="12.024" y1="21.8706" x2="12.024" y2="2.2851" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#BDBDBD"/>
+                      <stop offset="1" stopColor="white"/>
+                    </linearGradient>
+                    <radialGradient id="paint1_radial_660_3534" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(12.0634 10.7622) scale(9.73919 9.79277)">
+                      <stop stopColor="#06C2E7"/>
+                      <stop offset="0.25" stopColor="#0DB8EC"/>
+                      <stop offset="0.5" stopColor="#12AEF1"/>
+                      <stop offset="0.75" stopColor="#1F86F9"/>
+                      <stop offset="1" stopColor="#107DDD"/>
+                    </radialGradient>
+                  </defs>
+                </svg>
               Safari Camera Permission Setup
             </span>
-              <span className={styles.accountInfoBoxClose} onClick={() => setShowSafariSetup(false)}>&times;</span>
+              <span className={styles.accountInfoBoxClose} onClick={() => setShowSafariSetup(false)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M12.0008 13.3998L7.10078 18.2998C6.91745 18.4831 6.68411 18.5748 6.40078 18.5748C6.11745 18.5748 5.88411 18.4831 5.70078 18.2998C5.51745 18.1165 5.42578 17.8831 5.42578 17.5998C5.42578 17.3165 5.51745 17.0831 5.70078 16.8998L10.6008 11.9998L5.70078 7.0998C5.51745 6.91647 5.42578 6.68314 5.42578 6.3998C5.42578 6.11647 5.51745 5.88314 5.70078 5.6998C5.88411 5.51647 6.11745 5.4248 6.40078 5.4248C6.68411 5.4248 6.91745 5.51647 7.10078 5.6998L12.0008 10.5998L16.9008 5.6998C17.0841 5.51647 17.3174 5.4248 17.6008 5.4248C17.8841 5.4248 18.1174 5.51647 18.3008 5.6998C18.4841 5.88314 18.5758 6.11647 18.5758 6.3998C18.5758 6.68314 18.4841 6.91647 18.3008 7.0998L13.4008 11.9998L18.3008 16.8998C18.4841 17.0831 18.5758 17.3165 18.5758 17.5998C18.5758 17.8831 18.4841 18.1165 18.3008 18.2998C18.1174 18.4831 17.8841 18.5748 17.6008 18.5748C17.3174 18.5748 17.0841 18.4831 16.9008 18.2998L12.0008 13.3998Z" fill="#6A6A61"/>
+                </svg>
+              </span>
           </div>
             <div className={`${styles.accountInfoBoxDesc} body2`}>
             Avoid repeated camera permission popups for seamless scanning on Nutrica.life, with Safari.
