4aaa5b844a8eea528875c9d7825a5b39807a35c4
Zoey <zhou22@seas.upenn.edu>
2025-08-26 13:04:03 -0400

    Update salmon.js

--- frontend/src/data/puzzles/salmon.js
+++ frontend/src/data/puzzles/salmon.js
@@ -1,4 +1,4 @@
-// avocado.js
+// salmon.js
 const B = { color: '#F3F3EC', nutrient: 0 };
 const C1 = { color: '#FFF5DD', nutrient: 1 }; // 碳水
 const C2 = { color: '#FFCAAC', nutrient: 1 }; // 碳水
@@ -41,31 +41,31 @@ const salmon = {
     // 7
     [B,B,B,B,B,B,B,B,P,C2,C3,C3,C3,C3,C2,C3,C3,C2,P,B,B,B,B,B],
     // 8
-    [B,B,B,B,B,B,B,P,C2,C3,C3,C3,C3,C2,C3,C3,C3,C2,P,B,B,B,B,B,B],
+    [B,B,B,B,B,B,B,P,C2,C3,C3,C3,C3,C2,C3,C3,C3,C2,P,B,B,B,B,B],
     // 9
-    [B,B,B,B,B,P,C2,C2,C3,C3,C3,C2,C3,C3,C3,C3,C2,P,B,B,B,B,B,B,B],
+    [B,B,B,B,B,B,P,C2,C2,C3,C3,C3,C2,C3,C3,C3,C3,C2,P,B,B,B,B,B],
     // 10
-    [B,B,B,P,C2,C4,C3,C2,C3,C1,C2,C4,C4,C3,C3,C2,P,B,B,B,B,B,B,B],
+    [B,B,B,B,B,P,C2,C4,C3,C2,C3,C1,C2,C4,C4,C3,C3,C2,P,B,B,B,B,B],
     // 11
-    [B,B,B,P,C2,C3,C3,C3,C1,C1,C1,C3,C2,C3,C2,F2,P,B,B,B,B,B,B,B],
+    [B,B,B,B,B,P,C2,C3,C3,C3,C1,C1,C1,C3,C2,C3,C2,F2,P,B,B,B,B,B],
     // 12
-    [B,B,B,P,C3,C3,C3,C3,C1,C1,C3,C3,C3,C2,C2,F2,P,B,B,B,B,B,B,B],
+    [B,B,B,B,B,P,C3,C3,C3,C3,C1,C1,C3,C3,C3,C2,C2,F2,P,B,B,B,B,B],
     // 13
-    [B,B,P,C4,C4,C3,C2,C5,C5,C3,C2,C4,C4,C2,F2,P,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,C4,C4,C3,C2,C5,C5,C3,C2,C4,C4,C2,F2,P,B,B,B,B,B,B],
     // 14
-    [B,B,P,C3,C3,C2,C5,C5,C2,C3,C3,C2,C2,F2,F2,P,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,C3,C3,C2,C5,C5,C2,C3,C3,C2,C2,F2,F2,P,B,B,B,B,B,B],
     // 15
-    [B,B,P,C2,C2,C5,C5,C2,C3,C3,C2,C2,F2,F2,P,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,C2,C2,C5,C5,C2,C3,C3,C2,C2,F2,F2,P,B,B,B,B,B,B,B],
     // 16
-    [B,B,P,C2,C5,C5,P,C3,C3,C2,F2,F2,F2,P,B,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,C2,C5,C5,P,C3,C3,C2,F2,F2,F2,P,B,B,B,B,B,B,B,B],
     // 17
-    [B,B,P,F1,C5,P,C2,C3,C2,F2,F2,P,P,B,B,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,F1,C5,P,C2,C3,C2,F2,F2,P,P,B,B,B,B,B,B,B,B,B],
     // 18
-    [B,B,P,F2,P,P,C2,C2,F1,F2,P,B,B,B,B,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,P,F2,P,P,C2,C2,F1,F2,P,B,B,B,B,B,B,B,B,B,B,B],
     // 19
-    [B,B,B,P,P,P,F1,F1,F1,P,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,B,P,P,P,F1,F1,F1,P,B,B,B,B,B,B,B,B,B,B,B,B],
     // 20
-    [B,B,B,B,B,P,P,P,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
+    [B,B,B,B,B,B,B,P,P,P,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
     // 21
     [B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
     // 22
@@ -74,7 +74,7 @@ const salmon = {
     [B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
     // 24
     [B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B],
-  ]      
+  ]   
 };
 
 export const salmonColorOrder = [C1.color, C2.color, C3.color, C4.color, C5.color, P.color, F1.color, F2.color];
